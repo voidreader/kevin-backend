7c5fd13f36ab8537336268af093a6d3641082115
gameless <radiogaga.jin@gmail.com>
2023-11-02 14:11:04 +0900

    Update time-transformer.ts

--- src/util/time-transformer.ts
+++ src/util/time-transformer.ts
@@ -2,38 +2,42 @@ import { ValueTransformer } from 'typeorm';
 
 export class LocalDateTimeTransformer implements ValueTransformer {
   changeDateFormat(date: Date) {
-    let month: number | string = date.getMonth() + 1;
-    let day: number | string = date.getDate();
-    let hour: number | string = date.getHours();
-    let minute: number | string = date.getMinutes();
-    let second: number | string = date.getSeconds();
+    try {
+      let month: number | string = date.getMonth() + 1;
+      let day: number | string = date.getDate();
+      let hour: number | string = date.getHours();
+      let minute: number | string = date.getMinutes();
+      let second: number | string = date.getSeconds();
 
-    month = month >= 10 ? month : '0' + month;
-    day = day >= 10 ? day : '0' + day;
-    hour = hour >= 10 ? hour : '0' + hour;
-    minute = minute >= 10 ? minute : '0' + minute;
-    second = second >= 10 ? second : '0' + second;
+      month = month >= 10 ? month : '0' + month;
+      day = day >= 10 ? day : '0' + day;
+      hour = hour >= 10 ? hour : '0' + hour;
+      minute = minute >= 10 ? minute : '0' + minute;
+      second = second >= 10 ? second : '0' + second;
 
-    return (
-      date.getFullYear() +
-      '-' +
-      month +
-      '-' +
-      day +
-      ' ' +
-      hour +
-      ':' +
-      minute +
-      ':' +
-      second
-    );
+      return (
+        date.getFullYear() +
+        '-' +
+        month +
+        '-' +
+        day +
+        ' ' +
+        hour +
+        ':' +
+        minute +
+        ':' +
+        second
+      );
+    } catch (error) {
+      return date;
+    }
   }
 
   to(entityValue: Date): Date {
     return entityValue;
   }
 
-  from(databaseValue: Date): string {
+  from(databaseValue: Date): string | Date {
     return this.changeDateFormat(databaseValue);
   }
 }
